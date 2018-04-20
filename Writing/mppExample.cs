589914e1754ee9378d9d003f8c4ced5a6762158b
miketran238 <ngocmike238@gmail.com>
2018-04-20 12:26:12 -0500

    Add cs file

---
+++ Writing/mppExample.cs
@@ -0,0 +1,21 @@
+	// C# reference code: 
+	public bool IsSimilar ( String name ) {
+		int idx = name. IndexOf ( '[' );
+		for ( String n : part ) {
+			if (n. StartsWith ( name ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// C# code translated by mppSMT:
+	public bool IsSimilar( String name ) {
+		int idx = name. IndexOf ( '[' );
+		for ( int k = 0; k < part. Count ; ++k ) 
+			if ( part [ k ] . StartsWith ( name ) ) {
+			return true;
+			}
+		}
+		return false;
+	}
