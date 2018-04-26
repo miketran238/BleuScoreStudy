23e06efd14d7f64d2d6ee74cb7c853410cede55c
Tien N. Nguyen <tien@iastate.edu>
2018-04-26 16:49:12 -0500

    revising Hieu's section

--- Writing/samples.cs
+++ Writing/samples.cs
@@ -1,16 +1,13 @@
 	// Reference code
 	public void TestParserGrammar() {
-		string grammar = "grammar_T;\n" + "options_{output=AST;}\n"
-				+ "tokens_{BLOCK;}\n" + "a_:_lc='{'_ID+_'}'_->_^(BLOCK[$lc,\"block\"]_ID+)_;\n"
-				+ "type_:_'int'_|_'float'_;\n" ... ;
-		string found = execParser("T.g", grammar, "TParser", "TLexer", "a", "{a_b_c}", debug);
+		string grammar = "grammar_T;\n" + "options_{output=AST;}\n" + ...
+		string found = execParser("T.g", grammar, "TParser", "TLexer", ...;
 		Assert.AreEqual("(block_a_b_c)" + NewLine, found);
 	}
 	// Translated code
 	public void TestParserGrammar() {
-		string grammar = "grammar_T;\n" + "options_{output=AST;}\n" + "tokens_{BLOCK;}\n"
-				+ < unk > +"type_:_'int'_|_'float'_;\n" ... ;
-		string found = execParser("T.g", grammar, "TParser", "TLexer", "a", < unk >, debug );
+		string grammar = "grammar_T;\n" + ...;
+		string found = execParser("T.g", grammar, ...;
 		Assert.AreEqual( < unk > +NewLine, found );
 	}
 	// Reference code
