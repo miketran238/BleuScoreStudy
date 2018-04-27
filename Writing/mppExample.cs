59d84f7b94e6ddb14f65d143b40ad35b0f72361e
nguyenhoan <hoan@iastate.edu>
2018-04-27 04:15:04 -0500

    line number and code

--- Writing/mppExample.cs
+++ Writing/mppExample.cs
@@ -1,27 +1,27 @@
-	// C# reference code: 
-	public bool IsSimilar ( String name ) {
-		int idx = name. IndexOf ( '[' );
-		for ( String n : part ) {
-			if (n. StartsWith ( name ) ) {
+		// C# reference code: 
+		public bool IsSimilar ( String name ) {
+			int idx = name. IndexOf ( '[' );
+			for ( String n : part ) {
+				if (n. StartsWith ( name ) ) {
+					return true;
+				}
+			}
+			return false;
+		}
+		// C# code translated by mppSMT:
+		public bool IsSimilar( String name ) {
+			int idx = name. IndexOf ( '[' );
+			for ( int k = 0; k < part. Count ; ++k ) 
+				if ( part [ k ] . StartsWith ( name ) ) {
 				return true;
+				}
 			}
+			return false;
 		}
-		return false;
-	}
-	// C# code translated by mppSMT:
-	public bool IsSimilar( String name ) {
-		int idx = name. IndexOf ( '[' );
-		for ( int k = 0; k < part. Count ; ++k ) 
-			if ( part [ k ] . StartsWith ( name ) ) {
-			return true;
-			}
-		}
-		return false;
-	}
-	// C# code translated by p-mppSMT
-	public bool } String name ) {
-		int idx = name. IndexOf ( '[' ); 
-		for ( . ] k [ part ( if ) ++k ; 
-		Count part. < k 0; = k int StartsWith ( name ) ) 
-		{ return true; } } 
-		return false; IsSimilar(
+		// C# code translated by p-mppSMT
+		public bool } String name ) {
+			int idx = name. IndexOf ( '[' ); 
+			for ( . ] k [ part ( if ) ++k ; 
+			Count part. < k 0; = k int StartsWith ( name ) ) 
+			{ return true; } } 
+			return false; IsSimilar(
